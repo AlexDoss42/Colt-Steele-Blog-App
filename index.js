b87325a15491eb5c4ec76634851843ea850dc055
Alex Doss <alex.doss42@gmail.com>
2020-05-18 21:31:31 -0600

    Sets up Express server

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ var express = require("express"),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
     app = express();
+
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.listen(3005, () => console.log(`It's over Anakin. I have the high ground`));
