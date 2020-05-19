6f8893f4ea28144ff8594bc8f09019d0e3d949e4
Alex Doss <alex.doss42@gmail.com>
2020-05-18 21:33:19 -0600

    Sets up blog schema

--- index.js
+++ index.js
@@ -8,5 +8,12 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+var blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {type: Date, default: Date.now}
+})
+
 
 app.listen(3005, () => console.log(`It's over Anakin. I have the high ground`));
