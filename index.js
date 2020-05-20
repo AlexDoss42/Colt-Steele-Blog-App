c1fefe1924cf94521d08d8d9f654bc54efdcac9e
Alex Doss <alex.doss42@gmail.com>
2020-05-19 21:35:47 -0600

    Adds create endpoint

--- index.js
+++ index.js
@@ -33,6 +33,18 @@ app.get("/blogs", function(req, res){
 
 app.get("/blogs/new", function(req, res){
   res.render("new");
-})
+});
+
+app.post("blogs", function(req, res){
+
+  Blog.create(req.body.blog, function(err, newBlog){
+    if(err){
+      console.log(err);
+      res.render("new")
+    } else {
+      res.redirect("/blogs");
+    }
+  });
+});
 
 app.listen(3005, () => console.log(`It's over Anakin. I have the high ground`));
